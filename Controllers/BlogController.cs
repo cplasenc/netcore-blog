a7a66655eaab080ffdd911198f8aa36ed6032343
Cristian Plasencia <plc.cristian@gmail.com>
2021-09-09 21:34:00 +0100

    blog controller

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netcore_blog.Controllers
+{
+    public class BlogController: Controller
+    {
+        private readonly IBlogServices blogServices;
+        public BlogController(IBlogServices blogServices)
+        {
+            _blogServices = blogServices;
+        }
+        public IActionResult Index()
+        {
+            var post = _blogServices.GetLatestPost(10);
+            return View("Index", post);
+        }
+    }
+}
